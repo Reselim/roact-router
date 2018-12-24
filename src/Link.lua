65b51d3d3a9f0d0b1c6988fd3cbde5f81dc8cfcd
Reselim <reselimiscool@gmail.com>
2018-12-24 18:31:36 -0500

    Use an ImageLabel for the Link component instead of a Frame

--- src/Link.lua
+++ src/Link.lua
@@ -3,18 +3,15 @@ local Roact = require(script.Parent.Parent.Roact)
 local Link = Roact.PureComponent:extend("Link")
 
 function Link:render()
-	return Roact.createElement("Frame", {
-		Active = false,
+	return Roact.createElement("ImageButton", {
 		Size = UDim2.new(1, 0, 1, 0),
 		BackgroundTransparency = 1,
 		
-		[Roact.Event.InputBegan] = function(_, input)
-			if input.UserInputType == Enum.UserInputType.MouseButton1 then
-				if self.props.replace then
-					self._context.history:replace(self.props.to)
-				else
-					self._context.history:push(self.props.to)
-				end
+		[Roact.Event.MouseButton1Down] = function()
+			if self.props.replace then
+				self._context.history:replace(self.props.to)
+			else
+				self._context.history:push(self.props.to)
 			end
 		end
 	})
