929b8d6d2e2dd358602ad1f17dcd323662c7cc50
Reselim <notreselim@gmail.com>
2020-10-14 17:10:57 -0400

    Update init file

--- src/init.lua
+++ src/init.lua
@@ -4,5 +4,8 @@ return {
 	Redirect = require(script.Redirect),
 
 	Context = require(script.Context),
+
+	-- Not intended for common use, but available anyway
+	Path = require(script.Path),
 	History = require(script.History)
 }
