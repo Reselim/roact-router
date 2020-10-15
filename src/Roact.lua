9fb5b895744f6bcf02671dade2734664428db9df
Reselim <notreselim@gmail.com>
2020-10-14 22:31:44 -0400

    Add human-readable error to Roact reference

--- src/Roact.lua
+++ src/Roact.lua
@@ -1,1 +1,8 @@
-return require(script.Parent.Parent.Roact)
+local container = script.Parent.Parent
+local roactModule = container:FindFirstChild("Roact")
+
+if not roactModule then
+	error("Roact Router failed to find Roact. Did you make sure Roact is in the same folder?")
+end
+
+return require(roactModule)
