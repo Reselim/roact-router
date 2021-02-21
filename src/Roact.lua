e5f90fe46e0d16d026654cd92068d6fdf165c3f1
Ashley Davies <ashley@davies.me.uk>
2021-02-21 02:26:21 +0000

    Update Roact.lua

--- src/Roact.lua
+++ src/Roact.lua
@@ -1,8 +1,18 @@
 local container = script.Parent.Parent
 local roactModule = container:FindFirstChild("Roact")
+local Roact = nil
 
 if not roactModule then
-	error("Roact Router failed to find Roact. Did you make sure Roact is in the same folder?")
+	-- If the rbx-ts runtime is around, try loading with that
+	local rbxts = game:GetService("ReplicatedStorage"):FindFirstChild("rbxts_include")
+	if rbxts ~= nil then
+		local TS = require(game:GetService("ReplicatedStorage"):WaitForChild("rbxts_include"):WaitForChild("RuntimeLib"))
+		Roact = TS.import(script, TS.getModule(script, "roact").roact.src)
+	else
+		error("Roact Router failed to find Roact. Did you make sure Roact is in the same folder?")
+	end
+else
+	Roact = require(roactModule)
 end
 
-return require(roactModule)
+return Roact
