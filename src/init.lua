ba9aa3a853f8677c933818b54bc3c0e22965fda8
Reselim <notreselim@gmail.com>
2021-04-04 05:23:15 -0400

    Add RoactRouter.with

--- src/init.lua
+++ src/init.lua
@@ -1,12 +1,22 @@
+local Roact = require(script.Roact)
+local Context = require(script.Context)
+
+local function with(callback)
+	return Roact.createElement(Context.Consumer, {
+		render = callback
+	})
+end
+
 return {
 	Link = require(script.Link),
 	Router = require(script.Router),
 	Route = require(script.Route),
 	Redirect = require(script.Redirect),
 
-	Context = require(script.Context),
-
 	-- Not intended for common use, but available anyway
 	Path = require(script.Path),
-	History = require(script.History)
+	History = require(script.History),
+
+	Context = Context,
+	with = with
 }
