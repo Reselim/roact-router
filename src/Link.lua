57321e5dc6506759299b20a22acdb15108249200
Reselim <notreselim@gmail.com>
2021-06-22 03:06:37 -0400

    Fix reference to Context in lesser used components

--- src/Link.lua
+++ src/Link.lua
@@ -1,31 +1,29 @@
 local Roact = require(script.Parent.Roact)
-local Context = require(script.Parent.Context)
+local withRouter = require(script.Parent.withRouter)
 local merge = require(script.Parent.merge)
 
 local function Link(props)
-	return Roact.createElement(Context.Consumer, {
-		render = function(context)
-			local elementProps = merge(props)
+	return withRouter(function(router)
+		local elementProps = merge(props)
 
-			-- Explicitly exclude props we don't want passed to the ImageButton
-			elementProps.path = nil
-			elementProps.state = nil
-			
-			return Roact.createElement("ImageButton", merge(elementProps, {
-				BackgroundTransparency = 1,
+		-- Explicitly exclude props we don't want passed to the ImageButton
+		elementProps.path = nil
+		elementProps.state = nil
+		
+		return Roact.createElement("ImageButton", merge(elementProps, {
+			BackgroundTransparency = 1,
 
-				[Roact.Event.Activated] = function()
-					local history = context.history
+			[Roact.Event.Activated] = function()
+				local history = router.history
 
-					if props.replace then
-						history:replace(props.path, props.state)
-					else
-						history:push(props.path, props.state)
-					end
-				end,
-			}))
-		end,
-	})
+				if props.replace then
+					history:replace(props.path, props.state)
+				else
+					history:push(props.path, props.state)
+				end
+			end,
+		}))
+	end)
 end
 
 return Link
--- src/Redirect.lua
+++ src/Redirect.lua
@@ -1,18 +1,15 @@
-local Roact = require(script.Parent.Roact)
-local Context = require(script.Parent.Context)
+local withRouter = require(script.Parent.withRouter)
 
 local function Redirect(props)
-	return Roact.createElement(Context.Consumer, {
-		render = function(context)
-			local history = context.history
-			
-			if props.replace then
-				history:replace(props.path, props.state)
-			else
-				history:push(props.path, props.state)
-			end
-		end,
-	})
+	return withRouter(function(router)
+		local history = router.history
+		
+		if props.replace then
+			history:replace(props.path, props.state)
+		else
+			history:push(props.path, props.state)
+		end
+	end)
 end
 
 return Redirect
