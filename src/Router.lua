3a5254e0d9106e5252b13e5695b43778cec76d26
Reselim <notreselim@gmail.com>
2021-06-25 17:36:44 -0400

    Update RouterContext when history location changes

--- src/Router.lua
+++ src/Router.lua
@@ -7,12 +7,25 @@ local Router = Roact.Component:extend("Router")
 
 function Router:init()
 	self.history = self.props.history or History.new(self.props.initialEntries, self.props.initialIndex)
+
+	local function updateLocation()
+		self:setState({
+			location = self.history.location,
+		})
+	end
+
+	updateLocation()
+	self.listener = self.history.onChanged:connect(updateLocation)
+end
+
+function Router:willUnmount()
+	self.listener:disconnect()
 end
 
 function Router:render()
 	return Roact.createElement(RouterContext.Provider, {
 		value = {
-			location = self.history.location,
+			location = self.state.location,
 			history = self.history,
 		},
 	}, self.props[Roact.Children])
