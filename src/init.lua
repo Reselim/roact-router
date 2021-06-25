98d2727fbfef67626f90f71fb730e0ca33febdb0
Reselim <notreselim@gmail.com>
2021-06-25 03:28:51 -0400

    ...actually export hooks

--- src/init.lua
+++ src/init.lua
@@ -9,6 +9,11 @@ return {
 	withRouter = require(script.withRouter),
 	withRoute = require(script.withRoute),
 
+	useHistory = require(script.useHistory),
+	useLocation = require(script.useLocation),
+	useParams = require(script.useParams),
+	useRouteMatch = require(script.useRouteMatch),
+
 	-- Not intended for common use, but available anyway
 	Path = require(script.Path),
 	History = require(script.History),
