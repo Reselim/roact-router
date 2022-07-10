9cdd203a1ae7f770136e7001e6a664a178ce0e59
Reselim <notreselim@gmail.com>
2022-07-10 06:04:33 -0400

    Update Roact.lua

--- src/Roact.lua
+++ src/Roact.lua
@@ -4,12 +4,12 @@ local Roact = container:FindFirstChild("Roact")
     or container.Parent:FindFirstChild("Roact")
     or container.Parent:FindFirstChild("roact")
 
-if Roact and Roact:IsA("Folder") then
-    Roact = Roact:FindFirstChildOfClass("ModuleScript")
-end
-
 if not Roact then
     error("Roact Router failed to find Roact. Did you make sure Roact is in the same folder?")
+end
+
+if Roact:IsA("Folder") then
+    Roact = Roact:FindFirstChildOfClass("ModuleScript")
 end
 
 return require(Roact)
